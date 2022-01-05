4be9beb25f94e968956c188443c5ebad299caeca
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2022-01-05 12:28:46 -0600

    Implemented cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: {origin: '*'}
+});
+const cors = require('cors');
 require('dotenv').config();
+
 const PORT = process.env.PORT;
 
 http.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
