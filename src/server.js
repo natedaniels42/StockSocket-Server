c7015917b0cb6f826dbb15a31fa9591bcc7d71e7
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2022-01-06 09:18:37 -0600

    changes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const cors = require('cors');
 const io = require('socket.io')(http, {
     cors: {origin: '*'}
 });
-const cors = require('cors');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+
+io.on('connection', (socket) => {
+    console.log('Connected')
+
+    socket.on('list', (data) => {
+        console.log('list')
+        io.emit('list', 'test')
+    })
+})
 
 http.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
