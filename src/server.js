f31ec31a0cc52995fc0112a8a0fd1c17fa4b8931
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2022-01-10 15:47:54 -0600

    historical request is running and connected to the client

--- src/server.js
+++ src/server.js
@@ -12,40 +12,18 @@ const { Stock } = require('./Stock');
 const symbols = data.stocks.map(stock => stock.symbol); 
 let index = 1;
 
-// console.log(data.stocks[0].data[data.stocks[0].data.length - 1]);
 setInterval(() => {
     data.updateData();
-    console.log(data.stocks[0].data[data.stocks[0].data.length - 1]);
 }, 2000);
-// let candlestick = new CandlestickStockData();
-// candlestick.populateData(5, [ new Stock('ATT',[
-//     {timestamp: 1, amount: 2},
-//     {timestamp: 1, amount: 5},
-//     {timestamp: 1, amount: 4},
-//     {timestamp: 1, amount: 8},
-//     {timestamp: 1, amount: 6},
-//     {timestamp: 1, amount: 1},
-//     {timestamp: 1, amount: 40},
-//     {timestamp: 1, amount: 44},
-//     {timestamp: 1, amount: 20},
-//     {timestamp: 1, amount: 26},
-//     {timestamp: 1, amount: 54},
-//     {timestamp: 1, amount: 1},
-//     {timestamp: 1, amount: 22},
-//     {timestamp: 1, amount: 18},
-//     {timestamp: 1, amount: 5}
-//     ])
-// ]
-// )
-// console.log(candlestick.stocks[0].data);
 
 
 io.on('connection', (socket) => {
     console.log('Connected');
 
     socket.on('historical', (sentData) => {
-        console.log(sentData);
-        io.emit('historical', {'response-type': 'historical', 'data': sentData})
+        const newData = new CandlestickStockData();
+        newData.populateData(sentData.data.interval, data.stocks);
+        io.emit('historical', {'response-type': 'historical', 'data': newData});
     })
 
     socket.on('live', (sentData) => {
